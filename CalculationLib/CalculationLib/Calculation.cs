f9e4ec28febc96746d6990736d1c2410a4c2a201
weddnesdday <131281854+weddnesdday@users.noreply.github.com>
2023-04-24 18:31:34 +0500

    Add files via upload

---
+++ CalculationLib/CalculationLib/Calculation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculationLib
+{
+    public static class Calculation
+    {
+        public static double Plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Divine(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ CalculationLib/CulculationApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculationLib;
+
+namespace CalculationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 6;
+            double b = 10;
+
+            Console.WriteLine("Деление:" + Calculation.Divine(a, b));
+            Console.WriteLine("Сложение:" + Calculation.Plus(a, b));
+            Console.WriteLine("Разность:" + Calculation.Minus(a, b));
+            Console.WriteLine("Умножение:" + Calculation.Multiply(a, b));
+
+        }
+    }
+}
